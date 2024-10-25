a31727dd5081e02470ee63460128ea6e819e712a
nethanielscaglione <zsweettop@gmail.com>
2024-10-25 12:50:36 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import netlify from "@astrojs/netlify";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "@playform/compress";
@@ -15,10 +14,7 @@ export default defineConfig({
   adapter: netlify({
     imageCDN: false,
   }),
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
-  // compatibility requirements for Cloudflare Pages
+  // compatibility requirements for Netlify
   vite: {
     ssr: {
       external: ["stream", "util", "os", "fs", "svgo"],
